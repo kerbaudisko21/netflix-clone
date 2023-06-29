4ef50001efd6730d497b557892ac9d43bf8216d3
kerbaudisko21 <wilbertw99@gmail.com>
2023-06-30 00:02:23 +0700

    profiles screen and middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
+import {signOut, getSession} from 'next-auth/react'
+import {NextPageContext} from 'next'
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
   return (
     <>
-      <div className="text-2xl text-green-500">Netflix Clone</div>
     </>
   )
 }
