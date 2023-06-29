282f9057a405304a5ad9380fd80ea725568cdad9
kerbaudisko21 <wilbertw99@gmail.com>
2023-06-29 23:28:51 +0700

    login register auth jwt

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+
+import prismadb from '@/lib/prismadb'
+
+export default NextAuth ({
+    providers: [
+        Credentials({
+            id: 'credentials',
+            name: 'credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password',
+                }
+            },
+            async authorize (credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error ('Email and Password required');
+                }
+
+                const user = await prismadb.user.findUnique ({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if (!user || !user.hashedPassword) {
+                    throw new Error('Email does not exist!');
+                }
+
+                const isCorrectPassword = await compare(
+                    credentials.password, 
+                    user.hashedPassword);
+
+                if (!isCorrectPassword) {
+                    throw new Error('Inccorect password');
+                }
+
+                return user;
+            }
+        })
+    ],
+    pages: {
+        signIn: '/auth',
+    },
+    debug: process.env.NODE_ENV == 'development',
+    session: {
+        strategy: 'jwt'
+    },
+    secret: process.env.NEXTAUTH_SECRET
+})
